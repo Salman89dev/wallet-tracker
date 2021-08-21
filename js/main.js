f3c0dd5b0c7632b1c1b0e93ee2ea6a648b1efc60
Salman89dev <hmsalmansadiq@gmail.com>
2021-08-21 03:42:24 -0700

    authentication done

--- js/main.js
+++ js/main.js
@@ -18,13 +18,21 @@ var signUpFormSubmisson= async (e)=>
         var fullName=fullNameSignUp.value;
         var email=emailSignUp.value;
         var password=passwordSignUp.value;
-        var signedIn=await auth.createUserWithEmailAndPassword(email,password)
-        var signedInInfo={
-            fullName:fullName,
-            email:email,
+        if(fullName && email && password)
+        {
+            // create user
+            var {user:{uid}}=await auth.createUserWithEmailAndPassword(email,password)
+            console.log(uid)
+
+            // store info in firestore
+            var signedInInfo={
+                fullName:fullName,
+                email:email,
+                createdAt:new Date(),
+            }
+            await firestore.collection("users").doc(uid).set(signedInInfo)
+            // REDIRECT TO DASHBOARD PAGE
         }
-        await firestore.collection("users").doc(signedIn.user.uid).set(signedInInfo)
-        
     } catch (error) {
         console.log(error.message)
     }
@@ -37,7 +45,13 @@ var signInFormSubmisson= async (e)=>
     try {
         var email=emailSignIn.value;
         var password=passwordSignIn.value;
-        await auth.signInWithEmailAndPassword(email,password);
+        if(email && password)
+        {
+            var {user:{uid}} =await auth.signInWithEmailAndPassword(email,password);
+            console.log(uid)
+            var loggedInUserInfo= await firestore.collection("users").doc(uid).get();
+            console.log(loggedInUserInfo.data())
+        }
         console.log("done")
     } catch (error) {
         console.log(error.message)
@@ -49,3 +63,16 @@ signUpFormArea.addEventListener("submit",(e)=>signUpFormSubmisson(e) )
 
 
 signInFormArea.addEventListener("submit",(e)=> signInFormSubmisson(e) )
+
+
+// var {name,age,subject:{minor,major},subject}={
+//     name:"salman",
+//     age:24,
+//     subject:
+//     {
+//         minor:"maths",
+//         major:"english",
+//     }
+// }
+
+// console.log(name ,age ,minor ,major ,subject)
